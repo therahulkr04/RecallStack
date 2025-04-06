ab20dd59f8f6d9a3822409d89688abe9304c425b
therahulkr04 <therahulkr04@gmail.com>
2025-04-07 04:49:31 +0530

    Add Spring Boot Project Structure

---
+++ src/main/java/com/recallstack/RecallStack/RecallStackApplication.java
@@ -0,0 +1,13 @@
+package com.recallstack.RecallStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecallStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecallStackApplication.class, args);
+	}
+
+}
